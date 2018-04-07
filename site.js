b63548a5d698fa2e891e6ce851a791c89ee315c2
Jarronb <baileyjarron@gmail.com>
2018-04-06 21:48:12 -0500

    Created stlyrs for nav ul being visible

--- site.js
+++ site.js
@@ -1,9 +1,11 @@
 (function($){
-
   $('html').addClass('js').removeClass('no-js');
   $('#nav').before('<a id="js-nav" class="border-menu" href="#nav"></a>');
+  $('#js-nav').on('click', function(e) {
+    e.preventDefault();
+    $('#nav ul').toggleClass('nav-is-visible');
+  });
   $("#slideshow > figure:gt(0)").hide();
-
   setInterval(function() {
     $('#slideshow > figure:first')
       .fadeOut(1000)
@@ -12,5 +14,6 @@
       .end()
       .appendTo('#slideshow');
   }, 3000);
+
 
   })(jQuery);
