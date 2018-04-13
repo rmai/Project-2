1bc7cc52f7c13166f034ce03b4e13964b33edf63
Jarronb <baileyjarron@gmail.com>
2018-04-12 23:43:08 -0500

    Linted js

--- site.js
+++ site.js
@@ -1,7 +1,11 @@
+/* eslint-disable */
 /* https://github.com/madmurphy/cookies.js (GPL3) */
-var docCookies={getItem:function(e){return e?decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*"+encodeURIComponent(e).replace(/[\-\.\+\*]/g,"\\$&")+"\\s*\\=\\s*([^;]*).*$)|^.*$"),"$1"))||null:null},setItem:function(e,o,n,t,r,c){if(!e||/^(?:expires|max\-age|path|domain|secure)$/i.test(e))return!1;var s="";if(n)switch(n.constructor){case Number:s=n===1/0?"; expires=Fri, 31 Dec 9999 23:59:59 GMT":"; max-age="+n;break;case String:s="; expires="+n;break;case Date:s="; expires="+n.toUTCString()}return document.cookie=encodeURIComponent(e)+"="+encodeURIComponent(o)+s+(r?"; domain="+r:"")+(t?"; path="+t:"")+(c?"; secure":""),!0},removeItem:function(e,o,n){return this.hasItem(e)?(document.cookie=encodeURIComponent(e)+"=; expires=Thu, 01 Jan 1970 00:00:00 GMT"+(n?"; domain="+n:"")+(o?"; path="+o:""),!0):!1},hasItem:function(e){return!e||/^(?:expires|max\-age|path|domain|secure)$/i.test(e)?!1:new RegExp("(?:^|;\\s*)"+encodeURIComponent(e).replace(/[\-\.\+\*]/g,"\\$&")+"\\s*\\=").test(document.cookie)},keys:function(){for(var e=document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g,"").split(/\s*(?:\=[^;]*)?;\s*/),o=e.length,n=0;o>n;n++)e[n]=decodeURIComponent(e[n]);return e}};"undefined"!=typeof module&&"undefined"!=typeof module.exports&&(module.exports=docCookies);
-
+var docCookies={getItem:function(e){return e?decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*"+encodeURIComponent(e).replace(/[\-\.\+\*]/g, "\\$&")+"\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1"))||null:null;}, setItem:function(e, o, n, t, r, c){if(!e||/^(?:expires|max\-age|path|domain|secure)$/i.test(e)){return!1;}var s=""; if(n){switch(n.constructor){case Number:s=n===1/0?"; expires=Fri, 31 Dec 9999 23:59:59 GMT":"; max-age="+n; break; case String:s="; expires="+n; break; case Date:s="; expires="+n.toUTCString();}}return document.cookie=encodeURIComponent(e)+"="+encodeURIComponent(o)+s+(r?"; domain="+r:"")+(t?"; path="+t:"")+(c?"; secure":""), !0;}, removeItem:function(e, o, n){return this.hasItem(e)?(document.cookie=encodeURIComponent(e)+"=; expires=Thu, 01 Jan 1970 00:00:00 GMT"+(n?"; domain="+n:"")+(o?"; path="+o:""), !0):!1;}, hasItem:function(e){return!e||/^(?:expires|max\-age|path|domain|secure)$/i.test(e)?!1:new RegExp("(?:^|;\\s*)"+encodeURIComponent(e).replace(/[\-\.\+\*]/g, "\\$&")+"\\s*\\=").test(document.cookie);}, keys:function(){for(var e=document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/), o=e.length, n=0; o>n; n++){e[n]=decodeURIComponent(e[n]);}return e;}}; typeof module!=="undefined"&&typeof module.exports!=="undefined"&&(module.exports=docCookies);
+/* eslint-enable */
 (function($){
+  var items= [];
+  var toppings= [];
+  var pizza_options= [];
   $('html').addClass('js').removeClass('no-js');
   $('#nav').before('<a id="js-nav" class="border-menu" href="#nav"></a>');
   $('#js-nav').on('click', function(e) {
@@ -23,79 +27,76 @@ var docCookies={getItem:function(e){return e?decodeURIComponent(document.cookie.
     e.preventDefault();
     $('#pizza-customization-page h3 a').removeClass('selected');
     $('.options-content').removeClass('selected');
-    if ($(this).attr('href') === "#sauce-cheese-crust") { //disply chesse,sauce,etc
-        $('#sauce-cheese-crust h3 a').addClass('selected');
-        $('#pizza-options').addClass('selected');
+    if ($(this).attr('href') === "#sauce-cheese-crust") { // disply chesse,sauce,etc
+      $('#sauce-cheese-crust h3 a').addClass('selected');
+      $('#pizza-options').addClass('selected');
     } else if (($(this).attr('href') === "#to-toppings") ||
-        ($(this).attr('href') === "#toppings-content") ) { //display toppings
-        $('#toppings-section h3 a').addClass('selected');
-        $('#toppings-content').addClass('selected');
-      }
+        ($(this).attr('href') === "#toppings-content") ) { // display toppings
+      $('#toppings-section h3 a').addClass('selected');
+      $('#toppings-content').addClass('selected');
+    }
   });
 
   $('#pizza-customization-page li').on('click', function(e) {
     e.preventDefault();
     console.log(this);
-    if ($(this).attr('id') === 'veggie-toppings') {   //Veggie header
+    if ($(this).attr('id') === 'veggie-toppings') {   // Veggie header
       $('#veggie-toppings a[href="#veggies"]').addClass('selected');
       $('#meat-toppings a[href="#meats"]').removeClass('selected');
       $('#meat-toppings').removeClass('selected');
       $('#veggie-toppings').addClass('selected');
-    } else if ($(this).attr('id') === 'meat-toppings') { //meat topping header
-        $('#meat-toppings a[href="#meats"]').addClass('selected');
-        $('#veggie-toppings a[href="#veggies"]').removeClass('selected');
-        $('#veggie-toppings').removeClass('selected');
-        $('#meat-toppings').addClass('selected');
-    } else if ($(this).attr('class') === 'meat-item') { //meat item
-        $(this).children('h5').children('a').toggleClass('selected');
-        $(this).find('.topping-amount').toggleClass('selected');
-        if($('ol.topping-amount.selected li').has('li.selected')){
-          $(this).addClass('selected');
-        } else {
-            $(this).removeClass('selected');
-        }
-    } else if ($(this).attr('class') === 'veggie-item') { //veggie item
-        $(this).children('h5').children('a').toggleClass('selected');
-        $(this).find('.topping-amount').toggleClass('selected');
-        if($('ol.topping-amount.selected li').has('li.selected')){
-          $(this).addClass('selected');
-        } else {
-            $(this).removeClass('selected');
-        }
+    } else if ($(this).attr('id') === 'meat-toppings') { // meat topping header
+      $('#meat-toppings a[href="#meats"]').addClass('selected');
+      $('#veggie-toppings a[href="#veggies"]').removeClass('selected');
+      $('#veggie-toppings').removeClass('selected');
+      $('#meat-toppings').addClass('selected');
+    } else if ($(this).attr('class') === 'meat-item') { // meat item
+      $(this).children('h5').children('a').toggleClass('selected');
+      $(this).find('.topping-amount').toggleClass('selected');
+      if($('ol.topping-amount.selected li').has('li.selected')){
+        $(this).addClass('selected');
+      } else {
+        $(this).removeClass('selected');
+      }
+    } else if ($(this).attr('class') === 'veggie-item') { // veggie item
+      $(this).children('h5').children('a').toggleClass('selected');
+      $(this).find('.topping-amount').toggleClass('selected');
+      if($('ol.topping-amount.selected li').has('li.selected')){
+        $(this).addClass('selected');
+      } else {
+        $(this).removeClass('selected');
+      }
     } else if (($(this).is('ol.topping-amount.selected li'))) { // topping amount
-        $(this).siblings().removeClass('selected');
-        $(this).toggleClass('selected');
-    } else if ($(this).is($('.pizza-main-options li'))) { //pizza main option
       $(this).siblings().removeClass('selected');
       $(this).toggleClass('selected');
-    } else if ($(this).is($('.pizza-secondary-options li'))) { //pizza secondary options
+    } else if ($(this).is($('.pizza-main-options li'))) { // pizza main option
+      $(this).siblings().removeClass('selected');
+      $(this).toggleClass('selected');
+    } else if ($(this).is($('.pizza-secondary-options li'))) { // pizza secondary options
       $(this).siblings().removeClass('selected');
       $(this).toggleClass('selected');
     } else if ($(this).children('a').attr('href') === '#to-toppings') { // Topping button on chesse, suace page
       console.log('TO TOPPINGS');
-        $('#pizza-customization-page h3 a').removeClass('selected');
-        $('.options-content').removeClass('selected');
-        $('#toppings-section h3 a').addClass('selected');
-        $('#toppings-content').addClass('selected');
-        $('html,body').scrollTop(0);
+      $('#pizza-customization-page h3 a').removeClass('selected');
+      $('.options-content').removeClass('selected');
+      $('#toppings-section h3 a').addClass('selected');
+      $('#toppings-content').addClass('selected');
+      $('html,body').scrollTop(0);
     }
     //  $(this).addClass('selected');
   });
 
-  $('.button').on('click' , function(e) {
-    var items= [];
-    var toppings= [];
-    var pizza_options= [];
+  $('.button').on('click', function(e) {
     var pageID= $(this).closest('html').attr('id');
-    var pageClass= $(this).closest('html').attr('class');
     var cost= $(this).attr('value');
     var name= $(this).attr('name');
     var count = docCookies.keys().length-1;
-    var item_count = 0;
+    var item_count;
+    var item;
     e.preventDefault();
     if(pageID === 'drinks-page') {
       console.log('DRINKS');
-      var item= {
+      item= {
         type: 'Drinks',
         cost: cost,
         name: name
@@ -103,7 +104,7 @@ var docCookies={getItem:function(e){return e?decodeURIComponent(document.cookie.
       items.push(item);
     } else if (pageID === 'desserts-page') {
       console.log('DESSERTS');
-      var item= {
+      item= {
         type: 'Desserts',
         cost: cost,
         name: name
@@ -111,7 +112,7 @@ var docCookies={getItem:function(e){return e?decodeURIComponent(document.cookie.
       items.push(item);
     }else if (pageID === 'pasta-page') {
       console.log('PASTA');
-      var item= {
+      item= {
         type: 'Pasta',
         cost: cost,
         name: name
@@ -119,7 +120,7 @@ var docCookies={getItem:function(e){return e?decodeURIComponent(document.cookie.
       items.push(item);
     }else if ($(this).closest('html').hasClass('sides-page')) {
       console.log('SIDES');
-      var item= {
+      item= {
         type: 'Sides',
         cost: cost,
         name: name
@@ -127,7 +128,7 @@ var docCookies={getItem:function(e){return e?decodeURIComponent(document.cookie.
       items.push(item);
     }else if ($(this).closest('html').hasClass('wings-page')) {
       console.log('WINGS');
-      var item= {
+      item= {
         type: 'Wings',
         cost: cost,
         name: name
@@ -135,7 +136,7 @@ var docCookies={getItem:function(e){return e?decodeURIComponent(document.cookie.
       items.push(item);
     }else if ($(this).closest('html').hasClass('pizza-page')) {
       console.log('PIZZA');
-      var item= {
+      item= {
         type: 'Pizza',
         cost: cost,
         name: name
@@ -146,9 +147,9 @@ var docCookies={getItem:function(e){return e?decodeURIComponent(document.cookie.
     }else if ($(this).text() === "Customize") {
       console.log("CUSTOMIZE");
       window.location.href = '/order-now/index.html';
-    }else if ($(this).closest('li').attr('id') === "toppings-add-btn") {  //add cart btn
-      if($('.topping-amount li:nth-of-type(n+2)').hasClass('selected')) { //Grabbing selected toppings
-        var item= {
+    }else if ($(this).closest('li').attr('id') === "toppings-add-btn") {  // add cart btn
+      if($('.topping-amount li:nth-of-type(n+2)').hasClass('selected')) { // Grabbing selected toppings
+        item= {
           type: 'Pizza',
           cost: 10.50,
           name: 'Custom Pizza',
@@ -166,7 +167,7 @@ var docCookies={getItem:function(e){return e?decodeURIComponent(document.cookie.
         });
 
         console.log(toppings);
-      } else if ($('#pizza-options li').hasClass('selected')) { //Pizaa options
+      } else if ($('#pizza-options li').hasClass('selected')) { // Pizaa options
         $('.pizza-main-options li.selected').each(function (){
           var pizza_option= {
             name: '',
@@ -181,56 +182,52 @@ var docCookies={getItem:function(e){return e?decodeURIComponent(document.cookie.
         console.log(pizza_options);
       }
       //  if(($('.topping-amount li:nth-of-type(n+2).selected').closest('html h5')))
-      //window.location.href = '/food'-choices/index.html';
-  }
-  function setItem(item) {
-    var _item = item.type + ',' + item.name + ',' + item.cost;
-    console.log(count);
-    item_count++;
-    count++;
-    console.log(count.toString());
-    docCookies.setItem(count.toString(),_item);
-    console.log(_item);
-  }
-  function getItem(count) {
-    for(var i = 0; i <= count; i++) {
+      // window.location.href = '/food'-choices/index.html';
+    }
+    function setItem(item) {
+      var _item = item.type + ',' + item.name + ',' + item.cost;
       console.log(count);
-      var _count = count;
-      _count++;
+      item_count = item_count + 1;
+      count++;
+      console.log(count.toString());
+      docCookies.setItem(count.toString(), _item);
+      console.log(_item);
     }
-    console.log(docCookies.getItem(_count.toString()));
-  }
-  function removeItem(count) {
-    for(var i = 0; i < count; i++) {
-      var _count = count;
-      console.log(docCookies.removeItem(_count.toString()));
-      _count++;
-    }
-  }
-
-
-  });
-  var validPayment = function(form_array) {
-      // make sure the following fields are not empty
-      var isValid = true;
-
-      form_array[0].regex = /.*/;
-      form_array[1].regex = /^[^\s@]+@[^\s@]+$/;
-      form_array[2].regex = /^\d{5}$/;
-      form_array[3].regex = /^\d{15,16}$/;
-      form_array[4].regex = /^\d{2}\/\d{2}$/;
-      form_array[5].regex = /^\d{3,4}$/;
-
-      for(var i = 0; i < form_array.length; i++) {
-        if(!form_array[i].regex.test(form_array[i].value)) {
-          isValid = false;
-          logEvent("Empty or wrong " + form_array[i].name);
-        }
+    function getItem(count) {
+      var _count;
+      var i;
+      for(i = 0; i <= count; i++) {
+        console.log(count);
+        _count = count;
+        _count++;
       }
-      return isValid;
-    };
-    
-  })(jQuery);
+      console.log(docCookies.getItem(_count.toString()));
+    }
+  });
 
-  // console.log($(this).text());
-  // console.log($(this).attr('href'));
+  function validPayment(form_array) {
+    // make sure the following fields are not empty
+    var isValid = true;
+    var i = 0;
+
+    form_array[0].regex = /.*/;
+    form_array[1].regex = /^[^\s@]+@[^\s@]+$/;
+    form_array[2].regex = /^\d{5}$/;
+    form_array[3].regex = /^\d{15,16}$/;
+    form_array[4].regex = /^\d{2}\/\d{2}$/;
+    form_array[5].regex = /^\d{3,4}$/;
+
+    for(i = 0; i < form_array.length; i++) {
+      if(!form_array[i].regex.test(form_array[i].value)) {
+        isValid = false;
+
+      }
+    }
+    return isValid;
+  }
+  validPayment(items);
+
+})(jQuery);
+
+// console.log($(this).text());
+// console.log($(this).attr('href'));
